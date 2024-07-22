4de3e76b60e8b1a1dc29fd42f9ac8dc0ef5abe52
natha <nathancazatti3@gmail.com>
2024-07-22 05:04:59 -0300

    Adicionar arquivos de projeto.

---
+++ Controle/Models/ChamadoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Chamados.Models
+{
+    public class Chamado
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Solicitacao { get; set; } = string.Empty;
+        public string Responsavel { get; set; } = string.Empty;
+        public DateTime Inicio { get; set; }
+        public DateTime? Termino { get; set; }
+    
+    }
+}
